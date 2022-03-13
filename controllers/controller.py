9e9e5fbb03ec7382e8023ef1f7a7d4ffc5222c6b
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-03-13 17:05:02 +0000

    finished all extensions

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,15 +2,33 @@ from app import app
 from flask import render_template, request 
 from modules.player import Player
 from modules.game import Game
+import random
 
 @app.route("/game")
 def main_page():
     return render_template("main.html")
 
 @app.route("/game", methods=['POST'])
-def store_player_choice():
+def play_game():
     player1 = Player(request.form['player1_name'], request.form['player1_choice'])
     player2 = Player(request.form['player2_name'], request.form['player2_choice'])
     game = Game(player1, player2 )
     winner = game.compare()
     return render_template('result.html', winner=winner, player1=player1.name,player2=player2.name, player1_choice=player1.choice, player2_choice=player2.choice )
+
+@app.route("/welcome")
+def rules_page():
+    return render_template("welcome.html")
+
+@app.route("/play")
+def vs_com_page():
+    return render_template("play.html")
+
+@app.route('/play', methods=["POST"])
+def play_game_vs_com():
+    player = Player(request.form["player1_name"], request.form['player1_choice'])
+    com_choices = ['rock', 'paper', 'scissors']
+    com_choice = random.choice(com_choices)
+    game = Game(player, Player("Com", com_choice))
+    winner= game.compare()
+    return render_template('result.html', winner=winner, player1=player.name,player2='Com', player1_choice=player.choice, player2_choice=com_choice )
